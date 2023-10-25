1861b4501454bbc700ef6cf668feab3d5baeb2be
VeronicaFontana <fontana.veronica98@gmail.com>
2023-10-25 14:46:26 +0200

    [ADD] Form

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$parola = $_GET["parola"];
+$testo = $_GET["testo"];
 
 ?>
 
@@ -9,8 +11,22 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP BadWords</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-  
+<section class="container">
+  <form method="GET" action="arrival.html">
+    <div class="mb-3">
+      <label for="FormControlInput" class="form-label">Bad word</label>
+      <input name="parola" type="text" class="form-control" id="FormControlInput" placeholder="Inserisci parola da censurare">
+    </div>
+    <div class="mb-3">
+      <label for="FormControlTextarea" class="form-label">Inserisci il testo</label>
+      <textarea name="testo" class="form-control" id="FormControlTextarea" rows="3"></textarea>
+    </div> 
+    <input class="btn btn-primary" type="submit" value="Submit">
+  </form>
+</section>
 </body>
 </html>
